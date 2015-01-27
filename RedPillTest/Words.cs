f2ef30873e84060c254f028209f00e7b2712b8ec
jahanzaibkh <jahanzaib.rahman@gmail.com>
2015-01-27 22:24:45 +0500

    test added

---
+++ RedPillTest/Words.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RedPillTest.Redpill;
+
+namespace RedPillTest
+{
+    [TestClass]
+    public class Words
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Redpill.RedPillClient client = new RedPillClient("BasicHttpBinding_IRedPill");
+            string expected = "s'taC dnA lemaC";
+            Assert.AreEqual(expected, client.ReverseWords("Cat's And Camel"));
+        }
+    }
+}
